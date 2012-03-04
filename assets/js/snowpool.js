f29e9e49ac217c9cc523528b20fe052ce096a0b3
Patrick Davey <Patrick@egressive.com>
2012-03-04 17:01:58 +1300

    making it live for snowpool

--- assets/js/snowpool.js
+++ assets/js/snowpool.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-  $.getJSON("http://api.lvh.me:3000/countries.js?callback=?", function(data) {
+  if (window.localStorage.getItem("chosenCountry")){
+    //then move straight to that country's show
+    $.mobile.changePage("#country_pool_list","slide");
+    empty_and_refresh_carpools();
+  }
+  $.getJSON("http://api.snowpool.org/countries.js?callback=?", function(data) {
     if(data.length == 0){
       return;
     }
@@ -8,9 +13,13 @@ $(document).ready(function(){
     });
     $("#country_list").listview('refresh');
   });
-  $("a.country_click").live('click',function(){
+  $('#country_pool_list').live("pageshow", function(){ // or pageshow
+    //refresh the list of pools
+    empty_and_refresh_carpools();
+  });
+  function empty_and_refresh_carpools(){
     $("#append-pools").empty();
-    $.getJSON("http://api.lvh.me:3000/countries/"+$(this).attr('id')+".js?callback=?", function(data) {
+    $.getJSON("http://api.snowpool.org/countries/"+window.localStorage.getItem("chosenCountry")+".js?callback=?", function(data) {
       if(data.length == 0){
         $('<li data-role="list-divider">No current carpools</li>').appendTo("#append-pools");
         $("#append-pools").listview('refresh');
@@ -28,10 +37,14 @@ $(document).ready(function(){
       $("#append-pools").listview('refresh');
       return false;
     });
+  }
+  $("a.country_click").live('click',function(){
+    window.localStorage.setItem("chosenCountry" ,$(this).attr('id'));
+    return false;
   });
   $("a.pool_link").live('click',function(e){
     $("#pool_details").empty();
-      $.getJSON("http://api.lvh.me:3000/pools/"+$(this).data('identity')+".js?callback=?", function(data) {
+      $.getJSON("http://api.snowpool.org/pools/"+$(this).data('identity')+".js?callback=?", function(data) {
         $("#pool_details").append("<h2>Carpool to "+data.field+"</h2>");
         $("#pool_details").append("<strong>Date Leaving</strong><br/>"+data.start+"<br/>");
         $("#pool_details").append("<strong>Date Returning</strong><br/>"+data.endDisp+"<br/>");

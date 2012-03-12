ced99a0076c1f623f82efcabcd3de224cdc0255f
Patrick Davey <Patrick@egressive.com>
2012-03-13 07:11:09 +1300

    revert to 6ebbf8f9ac0c7f2ca011e3dcb8080bf25c6b2746

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,23 +1,4 @@
-function checkConnection() {
-    var networkState = navigator.network.connection.type;
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.NONE]     = 'No network connection';
-    return states[networkState];
-}
-
-
-
-
 $(document).bind("mobileinit", function(){
-  if (checkConnection() == "No network connection"){
-    alert("You don't have an internet connection, please exit the app and restart when you have connection");
-  }
   $.mobile.defaultPageTransition = 'none';
   $.mobile.defaultDialogTransition = 'none';
   
